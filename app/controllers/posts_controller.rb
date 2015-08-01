fde865eea57d9849a4f7d73485947a8de08d0056
Sahash's MAC <sahashsainju@Sahashs-MacBook-Pro.local>
2015-08-01 14:34:51 -0700

    crud set up

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,5 +11,38 @@ class PostsController < ApplicationController
 	post = params.require(:post).permit(:image, :title, :description)
 	Post.create(post)
     redirect_to "/posts"
+    
+  end
+  def show
+    	post_id = params[:id]
+    	@post = Post.find(post_id)
+    	render :show
     end
+  def edit
+  	post_id = params[:id]
+    @post = Post.find(post_id)
+    render :edit
+   	# redirect_to "/posts"
+  end
+  def update
+  	post_id = params[:id]
+    db_post = Post.find(post_id)
+    # updated plane data from form
+    post_params = params.require(:post).permit(:image, :title, :description)
+
+    if db_post.update_attributes(post_params)
+    	redirect_to "/posts"
+    else
+    	render :edit
+	end	
+  end
+  def destroy
+  	post_id = params[:id]
+    @post = Post.find(post_id)
+    @post.destroy
+    redirect_to "/posts"
+
+  end
+
+
 end
